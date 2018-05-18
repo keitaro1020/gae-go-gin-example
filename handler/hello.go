497e1eaa17a65eae35a9ef043f43577cdfcbb419
Keitaro Shishido <keitaro.shishido@inte.co.jp>
2018-05-18 16:45:17 +0900

    logging

--- handler/hello.go
+++ handler/hello.go
@@ -3,8 +3,13 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine"
 )
 
 func (h *Handler) Hello(c *gin.Context) {
+	ac := appengine.NewContext(c.Request)
+
+	log.Infof(ac, "hello!")
 	c.String(http.StatusOK, "hello world")
 }
